caf67e79aac1a509cfe7d756217076339e89ba79
li <lizhengpeng1133@gmail.com>
2015-09-24 23:35:21 +0800

    add templates base

--- uscloud/frontend/views.py
+++ uscloud/frontend/views.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from flask import Blueprint, render_template, redirect, request, url_for, flash
-from flask.ext.login import login_required, login_user
+from flask.ext.login import login_required, login_user, logout_user
 
 from .forms import LoginForm
 from ..user.models import User
 
 
 frontend = Blueprint('frontend', __name__)
+
+
+@frontend.route('/', methods=['GET'])
+def index():
+    return redirect(url_for('host.index'))
+
 
 @frontend.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,5 +23,15 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user, remember=form.remeber_me.data)
             return redirect(request.args.get('next') or url_for('host.index'))
-        # flash('Invalid username or password')
+        flash('Invalid username or password')
     return render_template('frontend/login.html', form=form)
+
+
+@frontend.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have logout')
+    return  redirect(url_for('frontend.login'))
+
+
